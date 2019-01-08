f560fb437d5148f1b933eb893d7db9b33e645c4e
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-08 18:13:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,23 @@
 
-# Write your code here.
-def take_a_number(katz_deli, name)
-  katz_deli << name
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+def line(katz_deli)
+  if katz_deli.length ==0
+      puts "The line is currently empty."
+  else
+      puts "hello #{katz_deli[0..-1].join(" , ")}"
+  end
 end
+line (katz_deli = [])
+
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length == 0
+      puts "The line is currently empty."
+  else
+      katz_deli.each_with_index{|name,index| puts "The line is currently:#{index+1} #{name}."} 
+  
+   end
+end
+
 
 def now_serving(katz_deli)
   if katz_deli.length == 0
@@ -11,21 +25,5 @@ def now_serving(katz_deli)
   else
     puts "Currently serving #{katz_deli.shift}."
   end
-
-end
-
-def line(katz_deli)
-  if katz_deli.length == 0
-    puts "The line is currently empty."
-  else
-    message="The line is currently:"
-
-    katz_deli.each_with_index do |value, index|
-      message += " #{index.to_i+1}. #{value}"
-    end
-
-    puts "#{message}"
-  end
-
 
 end
