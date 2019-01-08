a6ddb3eab2420ad9b30b63bbb90a6b73342cdbd3
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-08 18:12:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,20 @@
 def line(katz_deli)
-if katz_deli.length ==0
-puts "The line is currently empty."
-else
-puts "hello #{katz_deli[0..-1].join(" , ")}"
-end
+  if katz_deli.length ==0
+      puts "The line is currently empty."
+  else
+      puts "hello #{katz_deli[0..-1].join(" , ")}"
+  end
 end
 line (katz_deli = [])
 
+
 def take_a_number(katz_deli, name)
-    if katz_deli.length == 0
-    puts "The line is currently empty."
+  if katz_deli.length == 0
+      puts "The line is currently empty."
   else
-  katz_deli.each_with_index{|name,index| puts "The line is currently:#{index+1} #{name}."} 
+      katz_deli.each_with_index{|name,index| puts "The line is currently:#{index+1} #{name}."} 
   
-end
+   end
 end
 
 
