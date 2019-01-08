031336cbfc89a2084b8d6690d4ae45e661e86cb0
Mostafa Elfishawy <fisho_world@yahoo.com>
2019-01-08 13:39:20 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+if katz_deli.length ==0
+puts "The line is currently empty."
+else
+puts "hello #{katz_deli[0..-1].join(" , ")}"
+end
+end
+line (katz_deli = [])
+
+def take_a_number(katz_deli, name)
+  katz_deli.each_with_index{|name,index| puts "The line is currently:#{index+1} #{name}."} 
+  
+end
